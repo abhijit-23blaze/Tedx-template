34619a31df5f5f62c84c8ee87fdfc6ea1585ebf2
Abhijit Patil <69165799+abhijit-23blaze@users.noreply.github.com>
2025-01-15 12:27:05 +0530

    Update policies.jsx

--- src/components/policies.jsx
+++ src/components/policies.jsx
@@ -173,7 +173,7 @@ function policies() {
             Information Technology (Intermediaries guidelines) Rules, 2011 that
             require publishing the rules and regulations, privacy policy and
             Terms of Use for access or usage of domain name
-            [https://www.tedxiiitsricity.com/] (“Website”), including the
+            [https://www.tedxiiitsricity.com/], including the
             related mobile site and mobile application (hereinafter 3 referred
             to as “Platform”). Your use of the Platform and services and tools
             are governed by the following terms and conditions ("Terms of Use")
@@ -255,30 +255,9 @@ function policies() {
             enforceability, shall be governed by and construed in accordance
             with the laws of India. All disputes arising out of or in connection
             with these Terms shall be subject to the exclusive jurisdiction of
-            the courts in (Enter City and State) All concerns or communications
+            the courts in India All concerns or communications
             relating to these Terms must be communicated to us using the contact
-            information provided on this website. Shipping Policy Processing
-            Time All orders are delivered within 2-3 business days. Orders are
-            not shipped or delivered on weekends or holidays. If we are
-            experiencing a high volume of orders, shipments may be delayed by a
-            few days. Please allow additional days in transit for delivery. If
-            there will be a significant delay in the shipment of your order, we
-            will contact you via email or phone. Shipping Time Domestic
-            Shipping: Orders will be delivered within 3 to 4 weeks from the date
-            of order confirmation. Please note that delivery times may be
-            affected by factors beyond our control, such as weather conditions,
-            customs delays, and other unforeseen circumstances. Return and
-            Refund policy Return We have a 5-day return policy, which means you
-            have 5 days after receiving your item to request a return. Once the
-            return product is received it will be inspected and the return will
-            be approved within 2 days Refunds 5 We will notify you once we’ve
-            received and inspected your return, and let you know if the refund
-            was approved or not. If approved, you’ll be automatically refunded
-            on your original payment method within 10 business days. Please
-            remember it can take some time for your bank or credit card company
-            to process and post the refund too. If more than 15 business days
-            have passed since we’ve approved your return, please contact us at
-            tedx@iiits.in.
+            information provided on this website.
           </p>
         </div>
       </div>
